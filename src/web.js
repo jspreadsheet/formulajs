e35c3a8f9dcc35f9520c7f4fdde93ca2530b52f9
Nícolas Amaral <nicolasjesseam@gmail.com>
2023-08-01 12:00:09 -0300

    add FILTERXML function

--- src/web.js
+++ src/web.js
@@ -1,5 +1,7 @@
 import * as error from './utils/error.js'
 import * as utils from './utils/common.js'
+import xmldom from 'xmldom'
+import xpath from 'xpath'
 
 export function ENCODEURL(url) {
   if (arguments.length !== 1 || arguments[0] === undefined) {
@@ -13,4 +15,43 @@ export function ENCODEURL(url) {
   url = utils.parseString(url)
 
   return encodeURIComponent(url)
+}
+
+export function FILTERXML(xml, xp) {
+  if (arguments.length === 0 || arguments.length > 2) {
+    return error.na
+  }
+
+  const anyError = utils.anyError(xml, xp)
+
+  if (anyError) {
+    return anyError
+  }
+
+  const DOMParser = xmldom.DOMParser
+  const result = []
+  
+  try {
+    const doc = new DOMParser({
+      locator: {},
+      errorHandler: {
+        warning: function (w) { throw error.value },
+        error: function (e) { throw error.value },
+        fatalError: function (e) { throw error.value }
+      }
+    }).parseFromString(xml, "text/xml")
+    const nodes = xpath.select(xp, doc)
+
+    for (let i = 0; i < nodes.length; i++) {
+      result.push([nodes[i].value || nodes[i].firstChild.data])
+    }
+
+    if (result.length === 0) {
+      return error.value
+    }
+
+    return result
+  } catch (err) {
+    return error.value
+  }
 }
--- test/web.js
+++ test/web.js
@@ -20,4 +20,60 @@ describe('Text', () => {
     expect(web.ENCODEURL(25)).to.equal('25')
     expect(web.ENCODEURL(0.343)).to.equal('0.343')
   })
+
+  it('FILTERXML', () => {
+    const xml1 = `<catalog><cd><title>Blood on the Tracks</title><artist>Bob Dylan</artist></cd>
+      <cd><title>Hide your heart</title><artist>Bonnie Tyler</artist></cd>
+      <cd><title>Legiao Urbana</title><artist>Legiao Urbana</artist></cd></catalog>`
+
+    const xml2 = `<bookstore><book><title>Learning XML</title><author>Erik T. Ray</author><price>39.95</price></book>
+      <book><title>Narnia</title><author>C. S. Lewis</author><price>30.00</price></book>
+      <book><title>Harry Potter</title><author>J K. Rowling</author><price>29.99</price></book></bookstore>`
+
+    const xml3 = `<world><country name="USA"><city>New York</city><city>Los Angeles</city></country>
+      <country name="UK"><city>London</city><city>Manchester</city></country></world>`
+
+    const xml4 = `<company><employee id="1"><name>John Doe</name><manager id="2" /></employee>
+      <employee id="2"><name>Jane Smith</name><manager id="3" /></employee>
+      <employee id="3"><name>James Brown</name></employee></company>`
+
+    expect(web.FILTERXML(xml1, '//cd/title')).to.eql([['Blood on the Tracks'], ['Hide your heart'], ['Legiao Urbana']])
+    expect(web.FILTERXML(xml1, '//cd/artist')).to.eql([['Bob Dylan'], ['Bonnie Tyler'], ['Legiao Urbana']])
+    expect(web.FILTERXML(xml2, '//book[price<30]/title')).to.eql([['Harry Potter']])
+    expect(web.FILTERXML(xml2, '//book[price>30]/title')).to.eql([['Learning XML']])
+    expect(web.FILTERXML(xml2, '//book[price=30]/title')).to.eql([['Narnia']])
+    expect(web.FILTERXML(xml3, "/world/country[@name='USA']/city")).to.eql([['New York'], ['Los Angeles']])
+    expect(web.FILTERXML(xml3, "/world/country[@name='UK']/city")).to.eql([['London'], ['Manchester']])
+    expect(web.FILTERXML(xml4, "/company/employee[name='John Doe']/manager/@id")).to.eql([['2']])
+    expect(web.FILTERXML(xml4, "/company/employee[name='Michael Jackson']/manager/@id")).to.eql(error.value)
+    expect(web.FILTERXML(`<book><title>Learning XML</author></book>`, "//book/title")).to.eql(error.value)
+    expect(web.FILTERXML(`<book id=123><title>Learning XML</title></book>`, "//book/title")).to.eql(error.value)
+
+    expect(web.FILTERXML(xml1, '')).to.eql(error.value)
+    expect(web.FILTERXML(xml1, null)).to.eql(error.value)
+    expect(web.FILTERXML(xml1, undefined)).to.eql(error.value)
+    expect(web.FILTERXML(xml1, true)).to.eql(error.value)
+    expect(web.FILTERXML(xml1, false)).to.eql(error.value)
+    expect(web.FILTERXML(xml1, 4)).to.eql(error.value)
+    expect(web.FILTERXML(xml1, '4')).to.eql(error.value)
+    expect(web.FILTERXML(xml1, [['//cd/title', '//cd/title']])).to.eql(error.value)
+    expect(web.FILTERXML(xml1)).to.eql(error.value)
+    
+    expect(web.FILTERXML('', '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML(null, '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML(undefined, '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML(true, '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML(false, '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML([[xml1, xml1]], '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML(4, '//cd/title')).to.eql(error.value)
+    expect(web.FILTERXML('4', '//cd/title')).to.eql(error.value)
+
+    expect(web.FILTERXML()).to.eql(error.na)
+    expect(web.FILTERXML(xml1, '//cd/title', 2)).to.eql(error.na)
+
+    Object.values(error).forEach((err) => {
+      expect(web.FILTERXML(err, '//cd/title')).to.eql(err)
+      expect(web.FILTERXML(xml1, err)).to.eql(err)
+    })
+  })
 })
